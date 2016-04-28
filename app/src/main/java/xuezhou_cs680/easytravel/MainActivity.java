77fc6ff127c7f9a89766672a0e8c4c3fbbfde7e9
Xue Zhou <xuezhouapplication@gmail.com>
2016-04-28 13:17:03 -0400

    Initial commit

---
+++ app/src/main/java/xuezhou_cs680/easytravel/MainActivity.java
@@ -0,0 +1,128 @@
+package xuezhou_cs680.easytravel;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView img=null;
+    private MenuInflater menuInflater;
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btn4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+
+        img = (ImageView)findViewById(R.id.imageView);
+        img.setImageResource(R.drawable.earth);
+
+
+        btn1 = (Button)findViewById(R.id.imageButton);
+        btn2 = (Button)findViewById(R.id.imageButton2);
+        btn3 = (Button)findViewById(R.id.imageButton3);
+        btn4 = (Button)findViewById(R.id.imageButton4);
+
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        //ActionBar actionBar =getActionBar();
+        menuInflater = getMenuInflater();
+
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri;
+
+                uri = Uri.parse("http://www.bentley.edu");
+                startActivity(new Intent(Intent.ACTION_VIEW, uri));
+            }
+        });
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        Uri uri;
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.web:
+                uri = Uri.parse("http://www.bentley.edu");
+                startActivity(new Intent(Intent.ACTION_VIEW, uri));
+                return true;
+
+            case R.id.maps:
+                uri = Uri.parse("geo:0,0?q=175 Forest Street waltham ma");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+	        	/*For API18 and 19 Google Maps is not on Launch Pad
+             	 so should first check if Package is present to avoid app crashing.
+             	 The API 23 emulator using the Intel instruction set would also crash here
+             	 without first checking.
+            	*/
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);}
+                return true;
+
+            case R.id.dialer:
+                uri = Uri.parse("tel:8574004109");
+                startActivity(new Intent(Intent.ACTION_VIEW, uri));
+                return true;
+
+            case R.id.email:
+                uri = Uri.parse("mailto:hbentley@bentley.edu");
+                startActivity(new Intent(Intent.ACTION_SENDTO, uri));
+                return true;
+
+            case R.id.save:
+                //dud
+                return true;
+
+            case R.id.exit:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//class ends
